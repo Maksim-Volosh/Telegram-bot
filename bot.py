1c6948cb3a96e3d5556425eb44c2451cfc345e0d
Max <114184285+Maksim-Volosh@users.noreply.github.com>
2023-11-11 17:10:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ config_dict = get_default_config()
 config_dict['language'] = 'ru'
 
 API_TOKEN = 'YOU API TOKEN'
-owm = OWM('09af5e7b80f9e46e0e0e4d45548a555a', config_dict)
+owm = OWM('owm token', config_dict)
 
 
 # Configure logging
